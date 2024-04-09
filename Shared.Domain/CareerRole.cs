986e59a613932412da131a4b57181eb9b79b3ad3
Maflend <maks233online@gmail.com>
2024-04-09 23:54:46 +0300

    Добавил логику бустинга роли

--- Shared.Domain/CareerRole.cs
+++ Shared.Domain/CareerRole.cs
@@ -1,12 +1,62 @@
-﻿namespace Shared.Domain;
+﻿using EnumsNET;
+using System.ComponentModel;
+
+namespace Shared.Domain;
 
 public enum CareerRole
 {
-    Amateur = 0,
-    AmateurPlus = 1,
+    [Description("Опыт не ивестен")]
+    Unknown = 0,
+
+    [Description("Amateur")]
+    Amateur = 1,
+
+    [Description("Amateur+")]
+    AmateurPlus = 11,
+
+    [Description("Intern")]
     Intern = 2,
-    InterPlus = 3,
-    Junior = 4,
-    Middle = 5,
-    Senior = 6,
+
+    [Description("Intern+")]
+    InternPlus = 22,
+
+    [Description("Junior")]
+    Junior = 3,
+
+    [Description("Middle")]
+    Middle = 4,
+
+    [Description("Senior")]
+    Senior = 5,
+}
+
+public static class CareerRoleExtensions
+{
+    /// <summary>
+    /// Повысить роль по прохождению собеседования.
+    /// </summary>
+    /// <param name="role">Роль которую требуеются забустить.</param>
+    /// <returns>Следующую роль, которая выдается после прохождения собеседования.</returns>
+    /// <exception cref="ArgumentException">Роль нельзя забустить.</exception>
+    public static CareerRole BoostByInterview(this CareerRole role)
+    {
+        if(role is CareerRole.Amateur)
+            return CareerRole.AmateurPlus;
+
+        if (role is CareerRole.AmateurPlus)
+            return CareerRole.Intern;
+
+        if (role is CareerRole.Intern)
+            return CareerRole.InternPlus;
+
+        throw new ArgumentException("Роль нельзя забустить");
+    }
+
+    /// <summary>
+    /// Получить реальное название роли.
+    /// </summary>
+    public static string GetRealName(this CareerRole role)
+    {
+        return role.AsString(EnumFormat.Description)!;
+    }
 }
