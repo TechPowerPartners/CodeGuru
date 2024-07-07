ad18f51da3a4579959570db0d80e64046fdc58cf
Jutonish123 <jutonish123@mail.ru>
2024-07-08 00:41:55 +0300

    Яооо любитель собачек

--- src/Angular/src/app/app.component.ts
+++ src/Angular/src/app/app.component.ts
@@ -1,18 +1,22 @@
+import { Component } from '@angular/core';
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeroesComponent  } from './heroes/heroes.component';
-import { ChildComponent } from './child/child.component';
 import { FirstpageComponent } from "./firstpage/firstpage.component";
+import { FooterComponent } from "./footer/footer.component";
+import { TrainingComponent } from "./training/training.component";
+import { ArticlesComponent } from './articles/articles.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, ChildComponent, TuiRootModule, TuiDialogModule, TuiAlertModule, FirstpageComponent],
+  imports: [RouterOutlet, HeroesComponent, TuiRootModule, TuiDialogModule, TuiAlertModule, FirstpageComponent, FooterComponent, TrainingComponent, RouterLink, ArticlesComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
 })
+
 export class AppComponent {
   title = 'Code Guru';
 }
--- src/Angular/src/app/firstpage/firstpage.component.ts
+++ src/Angular/src/app/firstpage/firstpage.component.ts
@@ -1,46 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TuiActiveZoneModule, tuiIsString } from '@taiga-ui/cdk';
-import { TuiDataListModule, TuiDialogModule, TuiDropdownModule, TuiHostedDropdownModule, TuiNotificationModule, TuiRootModule, TuiSvgModule } from '@taiga-ui/core';
+import {
+  TuiDataListModule,
+  TuiHostedDropdownModule,
+  TuiSvgModule,
+} from '@taiga-ui/core';
 import { TuiTabsModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-firstpage',
   standalone: true,
   imports: [
-    TuiTabsModule,TuiHostedDropdownModule,TuiSvgModule,TuiDataListModule, TuiActiveZoneModule, TuiHostedDropdownModule,CommonModule
+    TuiTabsModule,
+    TuiHostedDropdownModule,
+    TuiSvgModule,
+    TuiDataListModule,
+    TuiActiveZoneModule,
+    TuiHostedDropdownModule,
+    CommonModule,
+    RouterLink,
   ],
   templateUrl: './firstpage.component.html',
   styleUrl: './firstpage.component.less',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FirstpageComponent {
   readonly collaborators = ['Carol Cleveland', 'Neil Innes'];
-	 
-	    readonly tabs = [
-	        'Статьи',
-          'Обучение',
-          'Вакансии'
-	    ];
-	 
-	    activeElement = String(this.tabs[0]);
-	 
-	    get activeItemIndex(): number {
-	        
-	 
-	        return this.tabs.indexOf(this.activeElement);
-	    }
-	 
-	    stop(event: Event): void {
-	        // We need to stop tab custom event so parent component does not think its active
-	        event.stopPropagation();
-	    }
-	 
-	    onClick(activeElement: string): void {
-	        this.activeElement = activeElement;
-	    }
-	 
-	    isString(tab: unknown): tab is string {
-	        return tuiIsString(tab);
-	    }
-	}
+
+  readonly tabs = ['Статьи', 'Обучение', 'Вакансии'];
+
+  activeElement = String(this.tabs[0]);
+
+  get activeItemIndex(): number {
+    return this.tabs.indexOf(this.activeElement);
+  }
+
+  stop(event: Event): void {
+    // We need to stop tab custom event so parent component does not think its active
+    event.stopPropagation();
+  }
+
+  onClick(activeElement: string): void {
+    this.activeElement = activeElement;
+  }
+
+  isString(tab: unknown): tab is string {
+    return tuiIsString(tab);
+  }
+}
