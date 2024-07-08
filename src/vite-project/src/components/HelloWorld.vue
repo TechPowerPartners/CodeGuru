191d16509c71ac55a8782fddfd36418e423dffd7
Jutonish123 <jutonish123@mail.ru>
2024-07-08 23:31:35 +0300

    создал проект на vue, добавил все записимости, подключил UI библиотеку

---
+++ src/vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Test from './test.vue';
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>feeew</h1>
+    <Test />
+  </div>
+</template>
+
+<style scoped>
+</style>
