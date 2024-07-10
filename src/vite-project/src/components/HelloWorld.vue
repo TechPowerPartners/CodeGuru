ec03cadb293091e4bd26c20156c62d9108d300b8
Jutonish123 <jutonish123@mail.ru>
2024-07-10 21:26:45 +0300

    test

--- src/vite-project/src/components/HelloWorld.vue
+++ src/vite-project/src/components/HelloWorld.vue
@@ -1,12 +1,9 @@
 <script setup>
-import Test from './test.vue';
+import Header from './Header.vue';
 </script>
 
 <template>
-  <div class="greetings">
-    <h1>feeew</h1>
-    <Test />
-  </div>
+    <Header />
 </template>
 
 <style scoped>
--- src/vite-project/src/main.js
+++ src/vite-project/src/main.js
@@ -1,13 +1,24 @@
-import './main.css'
+import "./main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import Aura from "@primevue/themes/aura";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+app.use(PrimeVue, {
+  theme: {
+      preset: Aura,
+      options: {
+          darkModeSelector: '.dark',
+          cssLayer: false
+      }
+  }
+});
+app.use(createPinia());
+app.use(router);
+app.mount("#app");
